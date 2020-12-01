fb07fda359085bd189a168fd37ed90c95d291f04
angelacaterina <angela.caterina@icloud.com>
2020-12-01 11:45:38 +0100

    revisione htlm
    i menù dovevano andare direttamente all'interno dei singoli link e non al di fuori

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,25 +4,15 @@
 // Dovete riprodurre solo la navbar ed i menu a tendina, non tutto il sito. Fate attenzione che in questo caso non abbiamo un solo menu a tendina ma piú di uno. Il menu deve diventare visibile quando facciamo hover  o click ( a vs discrezione ) su uno dei link principali nella navbar. Ogni link della navbar ha il suo menu nascosto che deve apparire.
 
 $(function() {
-    $('.navbar_right a').eq(0).hover(function() {
-        $('.d_none').removeClass('d_none');
-        $('.funzionalità div').addClass('active');
-    });
-
-    $('.navbar_right a').eq(1).hover(function() {
-        $('.abbonamenti').addClass('active');
-    });
-
-    $('.navbar_right a').eq(2).hover(function() {
-        $('.chiSiamo').addClass('active');
-    });
-    
-    $('.navbar_right a').eq(3).hover(function() {
-        $('.aiuto').addClass('active');
-    });
-
-    $('.card').mouseleave(function() {
-        $('.card').siblings().removeClass('active');
-        $('.funzionalità').addClass('d_none');
-    });
-});
+    var dropDown = $('li.dropdown');
+  
+    dropDown.hover(
+      function(){
+        $(this).children('ul.dropdown_menu').removeClass('d_none');
+      },
+  
+      function(){
+        $(this).children('ul.dropdown_menu').addClass('d_none');
+      }
+    )
+  });
